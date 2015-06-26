01285a5f7bc258f921c6645c67c8113b05567d6b
mengqingyan <m_527@qq.com>
2015-06-26 10:22:17 +0800

    test

---
+++ TestSpr/src/main/java/com/revencoft/example/service/webservice/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.revencoft.example.service.webservice;
+
+/**
+ * @author mengqingyan
+ * @version 
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
